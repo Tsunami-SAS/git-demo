c12510fdc600a06bd47d567bfd9098f9feaafa77
YangJX <yangjiaxing5151@163.com>
2022-11-01 09:35:46 +0800

    first commit

---
+++ src/main/java/jms/jx/git/GitTest.java
@@ -0,0 +1,11 @@
+package jms.jx.git;
+
+/**
+ * @Author: 杨佳兴
+ * @Date: 2022/11/1 9:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
