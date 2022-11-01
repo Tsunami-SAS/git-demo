0040a34be961986d21362d1c5ed4b37862cb87c8
YangJX <yangjiaxing5151@163.com>
2022-11-01 10:53:43 +0800

    hot-fix first commit

--- src/main/java/jms/jx/git/GitTest.java
+++ src/main/java/jms/jx/git/GitTest.java
@@ -3,11 +3,13 @@ package jms.jx.git;
 /**
  * @Author: 杨佳兴
  * @Date: 2022/11/1 9:16
+ * 狗肉、羊肉、牡蛎、海参、泥鳅、山药、核桃、板栗、枸杞子
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git! haha");
         System.out.println("生气就不播，舆论参杂评论区是逼脸鲁的多");
         System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
